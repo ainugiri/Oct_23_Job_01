b2eb73def7a3eb5197f612be0742ef3ec0e2a722
ainugiri <40005294+ainugiri@users.noreply.github.com>
2023-10-06 17:18:47 +0530

    Sample2 Added

---
+++ Sample2.java
@@ -0,0 +1,23 @@
+class Car{
+	int model;
+	String brandName;
+	int mil;
+	void start(){
+	}
+
+}
+class Sample2{
+
+public void display(){
+System.out.println("Hi");
+}
+
+public String createMaterialNumber(int x, int y, String abc){
+	int portNo = x;
+	int areaCode =y;
+	String serviceName = abc;
+	return abc;
+}
+
+
+}
